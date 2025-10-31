e0340b9389ea994919ec55c923099475e9b150c1
Erick Fernando Morales Gil <moralesgilfernando@gmail.com>
2025-10-31 01:34:30 -0400

    se agrego Hasta la carpeta11 video9 min 1654

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        @stack('css')
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
@@ -32,10 +34,16 @@
             <main>
                 {{ $slot }}
             </main>
+
+            <div class="mt-16">
+                @include('layouts.partials.app.footer')
+            </div>
         </div>
 
         @stack('modals')
 
         @livewireScripts
+
+        @stack('js')
     </body>
 </html>
